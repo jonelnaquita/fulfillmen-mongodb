54dc7004655eefe87d04a5efe2eaa0745feda96d
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2023-04-17 04:21:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, Response
-
 #from flask_ngrok import run_with_ngrok
 
 import pymongo
@@ -33,5 +32,4 @@ def webhook():
     return Response(status=200)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run() 
